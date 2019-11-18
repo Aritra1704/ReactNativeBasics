59592d224d5925292d78156197175772a2ee7749
Aritra <aritra1704@gmail.com>
2019-11-18 19:31:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import ColorScreen from './src/screens/ColorScreen';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => ColorScreen);
